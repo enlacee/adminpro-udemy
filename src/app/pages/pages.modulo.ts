b85e4535f7fa6bc2c7c384cb461f408be5e8fa72
pepe <pepe@pprios.com>
2018-05-13 00:56:38 -0500

    componente progressbar reutilizable con input, output & viewchild

--- src/app/pages/pages.modulo.ts
+++ src/app/pages/pages.modulo.ts
@@ -1,12 +1,14 @@
-import { NgModule } from "@angular/core";
-
+import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { PAGES_ROUTERS } from "./pages.routes";
+import { PAGES_ROUTERS } from './pages.routes';
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import { PAGES_ROUTERS } from "./pages.routes";
 		DashboardComponent,
 		ProgressComponent,
 		Graficas1Component,
+		IncrementadorComponent
 	],
 	exports: [
 		PagesComponent,
@@ -24,7 +27,8 @@ import { PAGES_ROUTERS } from "./pages.routes";
 	],
 	imports: [
 		SharedModule,
-		PAGES_ROUTERS
+		PAGES_ROUTERS,
+		FormsModule
 	]
 })
 export class PagesModule { }
